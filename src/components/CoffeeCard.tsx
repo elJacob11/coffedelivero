432200e49ebd99f954b7150d87495a46d916c8f8
elJacob11 <matusiakkuba11@gmail.com>
2023-04-24 23:46:53 +0200

    Adding tailwind to coffees

--- src/components/CoffeeCard.tsx
+++ src/components/CoffeeCard.tsx
@@ -18,18 +18,18 @@ interface CoffeeProps {
 }
 
 export default function CoffeeCard({coffee}: CoffeeProps) {
-  const coffeeDesign = " w-100 bg-[#F3F2F2] "
+  const coffeeDesign = " flex flex-col w-[90%] bg-[#F3F2F2] mt-[3rem] p-2 mb-8 "
   
   return (
-    <div className={`${coffeeDesign}`}>
-      <img src={`/coffees/${coffee.photo}`} alt="" />
-      <div>
+    <div className={`${coffeeDesign} text-center items-center rounded-tr-sm rounded-bl-sm rounded-br-3xl rounded-tl-3xl`}>
+      <img src={`/coffees/${coffee.photo}`} alt={`${coffee.photo}`} className=' -mt-16' />
+      <div className='mt-3 mb-2'>
         {coffee.tags.map(tag => (
-          <span key={tag}>{tag}</span>
+          <span className=' bg-[#F1E9C9] rounded-full px-[0.5rem] py-[0.25rem] text-[#C47F28] ml-2 uppercase font-bold text-[0.7rem]' key={tag}>{tag}</span>
         ))}
       </div>
-      <h3>{coffee.name}</h3>
-      <p>{coffee.description}</p>
+      <h3 className='font-baloo text-[1.3rem] font-bold text-[#403937] pb-1'>{coffee.name}</h3>
+      <p className='text-[#8D8686] font-normal text-[0.95rem]'>{coffee.description}</p>
       <div>
           {coffee.price}
       </div>
